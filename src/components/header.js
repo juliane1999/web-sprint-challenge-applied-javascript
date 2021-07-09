a61e3493428e8a9a5b59fae099e15dc697d17791
juliane1999 <lastjuliane@gmail.com>
2021-07-09 11:34:34 -0700

    completed the first part of task 1

--- src/components/header.js
+++ src/components/header.js
@@ -11,9 +11,13 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const headDiv = document.createElement('div')
+  const spanDate = document.createElement('span')
+  const h1Title = document.createElement('h1')
+  const spanTemp = document.createElement('span')
 }
 
-const head = document.querySelector('.header-container')
+
 
 const headerAppender = (selector) => {
   // TASK 2
